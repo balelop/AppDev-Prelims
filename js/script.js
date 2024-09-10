f42d0f3acdddd33e1cdc06bb819aee435db392e6
reginagolez <reginagolez>
2024-09-10 11:30:41 +0800

    scrolltotop created

--- js/script.js
+++ js/script.js
@@ -77,3 +77,22 @@ function toggleBlog() {
   popup.classList.toggle('active');
 }
 
+// COCO
+var scrollToTopBtn = document.getElementById("coco");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+};
+
+
